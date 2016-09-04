2a46255b25b58943a4e08b7f01b44370181c6904
micspring <subhafromslg123@gmail.com>
2016-09-05 00:34:58 +0530

    README Template

--- src/main/groovy/com/subha/oauth2/controllers/UserController.groovy
+++ src/main/groovy/com/subha/oauth2/controllers/UserController.groovy
@@ -1,8 +1,10 @@
 package com.subha.oauth2
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.provider.OAuth2Authentication
+import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/resources")
 public class UserController {
 
+
+    @Autowired
+    TokenStore tokenStore
+
     @RequestMapping(value = "/user",method = RequestMethod.POST)
     String getUser(Authentication authentication) {
        // println "***** ${authentication.getClass()}"
-        "***** ${authentication.getClass()} \n\n ************** I am Foing Grea for ${((OAuth2Authentication)authentication).userAuthentication.getClass()}"
+        "***** ${authentication.getClass()} \n\n ************** I am Foing Grea for ${((OAuth2Authentication)authentication).userAuthentication.getClass()} \n tokenStore:${tokenStore.getClass()}\n Approval Store: ${tokenStore?.approvalStore}"
     }
 
     @RequestMapping(value = "/user1",method = RequestMethod.POST)
